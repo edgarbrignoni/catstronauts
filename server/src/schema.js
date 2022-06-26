f010056c080948b1af76908229e0196a5e31ed6a
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-06-26 14:09:17 -0400

    We've updated our schema with new fields and the Module type, but we still need a way to query for a specific track by its ID. To do that, we'll need to add another entry point to our schema.
    In our schema.js file in the server/src folder, we can see that we only have a single entry point to our API so far: the tracksForHome field that returns a list of Tracks. Let's add another one called track, which will return a single Track.

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,6 +4,7 @@ const typeDefs = gql`
   type Query {
     "Get tracks array for homepage grid"
     tracksForHome: [Track!]!
+    track: Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
