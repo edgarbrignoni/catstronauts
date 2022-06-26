4821fecac6ed08d4d692ff227ac14a6d3d9f9724
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-06-26 15:12:04 -0400

    Let's build our track query. We'll call it GET_TRACK all caps, and use the gql template literal. Let's add an export keyword to the declaration so the query is available in our test cases later on.

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -2,6 +2,10 @@ import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import { Layout, QueryResult } from "../components";
 
+export const GET_TRACK = gql`
+    // our query goes here
+`;
+
 const Track = ({ trackId }) => {
   return <Layout></Layout>;
 };
