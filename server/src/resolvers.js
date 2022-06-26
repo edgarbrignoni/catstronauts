b00b9cac0baaa2d13ef53efff8e2bcaf520443f4
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-06-26 12:53:56 -0400

    Let's destructure authorId from the parent argument. Then, we'll call the getAuthor method from our dataSources.trackAPI method, passing in the authorId. Finally, we'll return the result.

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -6,7 +6,9 @@ const resolvers = {
     }
   },
   Track: {
-    author: (parent, _, { dataSources }) => {}
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    }
   }
 };
 
