9632600b69ceaf775ac891af7395ef00b8cd2393
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-06-26 14:43:34 -0400

    We'll destructure the first parameter to retrieve the id property from the parent, that's the id of the track. We don't need the args parameter, so that can be an underscore, and then destructure the third context parameter for the dataSources property.
    Inside, we can return the results of calling our dataSources.trackAPI.getTrackModules method, passing in the id for the track.
    
    And that's it for our resolver! With that, we've got our resolvers, our datasource and our schema all updated and ready to take on our new query.

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -11,6 +11,9 @@ const resolvers = {
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
+    },
+    modules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(id);
     }
   }
 };
