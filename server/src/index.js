b7eb8d72933286b71b48684281bafadceb5771b8
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-06-25 23:40:49 -0400

    get mock data

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ typeDefs, mocks: true });
 
 server.listen().then(() => {
   console.log(`
