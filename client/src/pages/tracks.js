3f1d6dee9e2d1c05a9809d5b004a092dbf118123
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-06-25 23:53:28 -0400

    defining a query

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout } from "../components";
+import { gql } from "@apollo/client";
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+/** TRACKS query to retrieve all tracks */
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
+
 const Tracks = () => {
   return <Layout grid> </Layout>;
 };
