cdf71b42859b1f1f6a60917991f316653f6d7613
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-06-26 15:12:35 -0400

    And now we could either build our query by hand, or, because we already did the job in Apollo Studio, let's head back there, copy the query in our Operations section and paste it in our GET_TRACK variable just between the backticks in the gql tag.

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -3,7 +3,27 @@ import { gql, useQuery } from "@apollo/client";
 import { Layout, QueryResult } from "../components";
 
 export const GET_TRACK = gql`
-    // our query goes here
+  query getTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      description
+    }
+  }
 `;
 
 const Track = ({ trackId }) => {
