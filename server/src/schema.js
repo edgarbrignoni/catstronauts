1aa2d22c50bd8b173f9da374ef00b1805e21f094
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-06-26 14:03:33 -0400

    Let's start with the mockup of the Track page our design team provided us with. Here we'll need to display more information about a track than what's contained in a card on the homepage.
    We'll start by updating the Track type. We'll add the description (which is a String) and the numberOfViews (an Integer). Oh, and let's not forget to give our fields nice comments!

--- server/src/schema.js
+++ server/src/schema.js
@@ -19,6 +19,10 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
   }
 
   "Author of a complete Track"
