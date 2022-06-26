6d59c8fd36001f0de97881d978074a7d73720e95
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-06-25 23:55:27 -0400

    executing with useQuery

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Layout } from "../components";
-import { gql } from "@apollo/client";
-
+import { useQuery, gql } from "@apollo/client";
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
@@ -25,7 +24,13 @@ export const TRACKS = gql`
 `;
 
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  if (loading) return "Loading...";
+
+  if (error) return `Error! ${error.message}`;
+
+  return <Layout grid>{JSON.stringify(data)}</Layout>;
 };
 
 export default Tracks;
