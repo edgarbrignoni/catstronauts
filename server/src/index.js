2c07af6aead2461b7bf513b63890cd0f30b4b798
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-06-26 13:04:39 -0400

    Next, just below our resolvers import, we'll require track-api, our data source file (extending RESTDataSource), and call it TrackAPI (note the PascalCase convention, as we're dealing with the class here).

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
+const TrackAPI = require("./datasources/track-api");
 
 const server = new ApolloServer({
   typeDefs,
