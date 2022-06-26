8d57622526d59c19a552077ef4022edc8c12fb3b
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-06-26 14:36:09 -0400

    Let's create a method called getTrackModules. It takes a trackId as a parameter. Inside, it'll make a get call to the track/${trackId}/modules endpoint and return the results.

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -17,6 +17,10 @@ class TrackAPI extends RESTDataSource {
   getTrack(trackId) {
     return this.get(`track/${trackId}`);
   }
+
+  getTrackModules(trackId) {
+    return this.get(`track/${trackId}/modules`);
+  }
 }
 
 module.exports = TrackAPI;
