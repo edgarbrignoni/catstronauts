66f4ee79b18a11693b299fdc6278fb5cdced716e
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-06-26 14:13:40 -0400

    In our case, we want to use the track's ID as our argument. Following the syntax rules, we'll add the parentheses, then the name id, with the type ID which is required.

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,7 +4,8 @@ const typeDefs = gql`
   type Query {
     "Get tracks array for homepage grid"
     tracksForHome: [Track!]!
-    track: Track
+    "Fetch a specific track, provided a track's ID"
+    track(id: ID!): Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
