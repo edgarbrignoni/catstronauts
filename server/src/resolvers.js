47a0cc80a5a6deee2217abda9c60ba30789e4a7f
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-06-28 21:24:39 -0400

    Next, let's return an object with all the properties needed to fulfill our schema. For now, we'll assume that the API call was successful, and worry about the error handling in the next lesson.

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -11,6 +11,13 @@ const resolvers = {
   Mutation: {
     incrementTrackViews: async (_, { id }, { dataSources }) => {
       const track = await dataSources.trackAPI.incrementTrackViews(id);
+
+      return {
+        code: 200,
+        success: true,
+        message: `Successfully incremented number of views for track ${id}`,
+        track
+      };
     }
   },
   Track: {
